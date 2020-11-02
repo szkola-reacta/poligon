12fd3b61513aef7e3477402fb552ae3a2919bbd9
patrykomiotek <patryk.omiotek@gmail.com>
2020-11-02 16:38:31 +0100

    binding events

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,15 +1,48 @@
 import React from 'react';
 
-function Form() {
-  const handleClick = () => console.log('React Button');
-  const handleSubmit = (event) => {
+class Form extends React.Component {
+  state = {
+    name: ''
+  }
+  // constructor(props) {
+  //   super(props);
+  //   // this.handleChange = this.handleChange.bind(this);
+  //   // this.handleClick = this.handleClick.bind(this);
+  //   // this.handleSubmit = this.handleSubmit.bind(this);
+  // }
+  handleSubmit(event) {
     event.preventDefault();
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <button onClick={handleClick}>React Button</button>
-    </form>
-  );
+  }
+  handleClick = (event) => {
+    console.log('handleClick: ', event);
+    console.log('React Button');
+  }
+  handleChange = (event) => {
+    // event.target.value
+    console.log('input value: ', event.target.value);
+    this.setState({ name: event.target.value });
+    console.log('this: ', this);
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <input type="text" onChange={this.handleChange} />
+        <button onClick={this.handleClick}>React Button</button>
+      </form>
+    );
+  }
 }
+
+// function Form() {
+//   const handleClick = () => console.log('React Button');
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <button onClick={handleClick}>React Button</button>
+//     </form>
+//   );
+// }
 
 export default Form;
