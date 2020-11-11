fdb007ddec4c73666cf612e577021a51278ee932
patrykomiotek <patryk.omiotek@gmail.com>
2020-11-11 17:56:29 +0100

    lifecycle methods

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -12,111 +12,121 @@ function useInput(initialValue = '') {
   return [value, handleChange];
 }
 
-function Registration() {
-  const [name, handleNameChange] = useInput('Patryk');
-  const [surname, handleSurnameChange] = useInput('Omiotek');
-  const [age, handleAgeChange] = useInput('35');
-  const surnameInput = useRef();
+// function Registration() {
+//   const [name, handleNameChange] = useInput('Patryk');
+//   const [surname, handleSurnameChange] = useInput('Omiotek');
+//   const [age, handleAgeChange] = useInput('35');
+//   const surnameInput = useRef();
 
-  const handleClick = () => console.log('React Button');
-  const handleFocusClick = () => {
-    surnameInput.current.focus();
-    surnameInput.current.value = 'Kowalski';
-    surnameInput.current.style.border = '#f00 1px solid';
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
-  return (
-    <div>
-      <div>
-        <div>Name: {name}</div>
-        <div>Surname: {surname}</div>
-        <div>Age: {age}</div>
-      </div>
-      <Form handleSubmit={handleSubmit}>
-        <div className="Form__group">
-          <Input
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={name}
-            handleChange={handleNameChange}
-          />
-        </div>
-        <div className="Form__group">
-          <input
-            type="text"
-            ref={surnameInput}
-            name="surname"
-            placeholder="Surname"
-            defaultValue={surname}
-            onChange={handleSurnameChange}
-          />
-        </div>
-        <div className="Form__group">
-          <Input
-            type="text"
-            name="age"
-            placeholder="Age"
-            value={age}
-            handleChange={handleAgeChange}
-          />
-        </div>
-        <div className="Form__group">
-          <button type="submit" onClick={handleClick}>Send</button>
-          <button type="button" onClick={handleFocusClick}>Focus</button>
-        </div>
-      </Form>
-    </div>
-  );
-}
-
-// class Form extends React.Component {
-//   state = {
-//     name: '',
-//     surname: '',
-//     age: ''
-//   }
-//   constructor(props) {
-//     super(props);
-//     this.surnameInput = React.createRef();
-//   }
-//   handleSubmit(event) {
+//   const handleClick = () => console.log('React Button');
+//   const handleFocusClick = () => {
+//     surnameInput.current.focus();
+//     surnameInput.current.value = 'Kowalski';
+//     surnameInput.current.style.border = '#f00 1px solid';
+//   };
+//   const handleSubmit = (event) => {
 //     event.preventDefault();
-//   }
-//   handleClick = (event) => {
-//     console.log('handleClick: ', event);
-//     console.log('React Button');
-//   }
-//   handleChange = (event) => {
-//     // event.target.name
-//     const keyName = event.target.name;
-//     this.setState({ [keyName]: event.target.value });
-//   }
-//   handleFocusClick = () => {
-//     this.surnameInput.current.focus();
-//     this.surnameInput.current.value = 'Kowalski';
-//     this.surnameInput.current.style.border = '#f00 1px solid';
-//   }
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit.bind(this)}>
-//         <div>
-//           <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <div>Name: {name}</div>
+//         <div>Surname: {surname}</div>
+//         <div>Age: {age}</div>
+//       </div>
+//       <Form handleSubmit={handleSubmit}>
+//         <div className="Form__group">
+//           <Input
+//             type="text"
+//             name="name"
+//             placeholder="Name"
+//             value={name}
+//             handleChange={handleNameChange}
+//           />
 //         </div>
-//         <div>
-//           <input type="text" ref={this.surnameInput} name="surname" placeholder="Surname" onChange={this.handleChange} />
+//         <div className="Form__group">
+//           <input
+//             type="text"
+//             ref={surnameInput}
+//             name="surname"
+//             placeholder="Surname"
+//             defaultValue={surname}
+//             onChange={handleSurnameChange}
+//           />
 //         </div>
-//         <div>
-//           <input type="text" name="age" placeholder="Age" onChange={this.handleChange} />
+//         <div className="Form__group">
+//           <Input
+//             type="text"
+//             name="age"
+//             placeholder="Age"
+//             value={age}
+//             handleChange={handleAgeChange}
+//           />
 //         </div>
-//         <button type="submit" onClick={this.handleClick}>Send</button>
-//         <button type="button" onClick={this.handleFocusClick}>Focus</button>
-//       </form>
-//     );
-//   }
+//         <div className="Form__group">
+//           <button type="submit" onClick={handleClick}>Send</button>
+//           <button type="button" onClick={handleFocusClick}>Focus</button>
+//         </div>
+//       </Form>
+//     </div>
+//   );
 // }
+
+class Registration extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      surname: '',
+      age: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.surnameInput = React.createRef();
+  }
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate');
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+  handleClick = (event) => {
+    console.log('handleClick: ', event);
+    console.log('React Button');
+  }
+  handleChange = (event) => {
+    // event.target.name
+    const keyName = event.target.name;
+    this.setState({ [keyName]: event.target.value });
+  }
+  handleFocusClick = () => {
+    this.surnameInput.current.focus();
+    this.surnameInput.current.value = 'Kowalski';
+    this.surnameInput.current.style.border = '#f00 1px solid';
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
+        </div>
+        <div>
+          <input type="text" ref={this.surnameInput} name="surname" placeholder="Surname" onChange={this.handleChange} />
+        </div>
+        <div>
+          <input type="text" name="age" placeholder="Age" onChange={this.handleChange} />
+        </div>
+        <button type="submit" onClick={this.handleClick}>Send</button>
+        <button type="button" onClick={this.handleFocusClick}>Focus</button>
+      </form>
+    );
+  }
+}
 
 export default Registration;
